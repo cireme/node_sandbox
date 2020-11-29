e4d4b3a7697e7b9db6ee3da23021926ed46fb4f2
Emeric Stophe <emeric.stophe@gmail.com>
2020-11-29 15:50:46 +0100

    fix(template): export template as public file

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
+/* Instantiate jQuery variables */
 const $document = $(document);
 const $inputSearch = $('#input-search');
 const $tableBody = $('#table-body');
 
-const templateProductRows = $('#template-rows').html();
+/* Load HTML templates */
+let templateProductRows;
+loadProductsTemplate();
 
+/* Declare events */
 $document.on('click', '#button-search', () => {
     searchData();
 });
@@ -12,6 +16,9 @@ $document.on('keyup', '#input-search', (key) => {
     searchData();
 });
 
+/**
+ * Function to search products
+ */
 function searchData() {
     const search = $inputSearch.val();
 
@@ -19,11 +26,18 @@ function searchData() {
 
     if(search) {
         $.get('/search?search=' + search, (products) => {
-            const template = Handlebars.compile(templateProductRows);
-            // execute the compiled template and print the output to the console
-            $tableBody.html(template({ products }));
+            $tableBody.html(templateProductRows({ products }));
         });
     } else {
         document.location = '';
     }
+}
+
+/**
+ * Function that loads the HTML products template
+ */
+function loadProductsTemplate() {
+    $.get('/template/table-products.html', (html) => {
+        templateProductRows = Handlebars.compile(html);
+    });
 }
